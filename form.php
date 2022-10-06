435420783e73820705f4faf24d31b4ab8f90724b
Meg <106317697+SkyriMeg@users.noreply.github.com>
2022-10-06 16:04:03 +0200

    Ajout secu côté front

--- form.php
+++ form.php
@@ -15,23 +15,23 @@
 
         <div>
             <label for="lastname">Nom :</label>
-            <input type="text" id="lastname" name="user_lastname">
+            <input type="text" id="lastname" name="user_lastname" required>
         </div>
         <div>
             <label for="firstname">Prénom :</label>
-            <input type="text" id="firstname" name="user_firstname">
+            <input type="text" id="firstname" name="user_firstname" required>
         </div>
         <div>
             <label for="email">E-mail :</label>
-            <input type="text" id="email" name="user_email">
+            <input type="email" id="email" name="user_email" required>
         </div>
         <div>
             <label for="phone">Téléphone :</label>
-            <input type="number" id="phone" name="user_phone">
+            <input type="number" id="phone" name="user_phone" required>
         </div>
 
         <label for="subject">Choisissez votre sujet:</label>
-        <select id="subject" name="subject">
+        <select id="subject" name="subject" required>
             <option value="">--Veuillez sélectionner--</option>
             <option value="cours de batterie">Cours de batterie</option>
             <option value="cours de guitare">Cours de guitare</option>
@@ -40,7 +40,7 @@
 
         <div>
             <label for="message">Message :</label>
-            <textarea id="user_message" name="user_message"></textarea>
+            <textarea id="user_message" name="user_message" required></textarea>
         </div>
         <div class="button">
             <button type="submit">Envoyer votre message</button>
